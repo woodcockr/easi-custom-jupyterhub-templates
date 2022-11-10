057e37b9661e54b112af9ddb5cb82244c35a73ff
Jonathan Hodge <jonathan.hodge@csirochile.cl>
2022-11-10 15:24:03 -0300

    add styling of allocation and resource selectors

--- extra-assets/js/easi-custom.js
+++ extra-assets/js/easi-custom.js
@@ -1,3 +1,36 @@
 (function ($) {
-  console.log('EASI custom JS testing');
+  // ALLOCATION BUTTONS
+  $("#allocation-select").parent().append("<div class='allocation-btns'></div>")
+
+  $("#allocation-select option").each(function() {
+    label = "<strong>"+$(this).text().replace(/\: /g, "</strong><br>")
+    var btn = $('<div class="btn btn-easi-toggle" data-value="'+$(this).val()+'">'+label+'</div>');
+    $('.allocation-btns').append(btn);
+  });
+  $("#allocation-select").hide()
+
+  $(document).on('click', '.allocation-btns .btn-easi-toggle', function() {
+      $('.btn-easi-toggle').removeClass('on');
+      $(this).addClass('on');
+      $("#allocation-select").val($(this).data("value"));
+  });
+
+  // RESOURCE BUTTONS
+  $("#resources-select").parent().append("<div class='resource-btns'></div>")
+
+  $("#resources-select option").each(function() {
+    label = $(this).text().replace(/\, /g, "<br>")
+    var btn = $('<div class="btn btn-easi-toggle" data-value="'+$(this).val()+'">'+label+'</div>');
+    $('.resource-btns').append(btn);
+  });
+  $("#resources-select").hide()
+
+  $(document).on('click', '.resource-btns .btn-easi-toggle', function() {
+      $('.btn-easi-toggle').removeClass('on');
+      $(this).addClass('on');
+      $("#resources-select").val($(this).data("value"));
+      showInfo();
+  });
+
+
 })(jQuery);
