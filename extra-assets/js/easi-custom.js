0a1930421b839cd6ef3999771a90ef7cb74df792
Jonathan Hodge <jonathan.hodge@csirochile.cl>
2022-12-14 22:28:54 -0300

    various updates including docs

--- extra-assets/js/easi-custom.js
+++ extra-assets/js/easi-custom.js
@@ -1,14 +1,14 @@
 (function ($) {
   // ALLOCATION BUTTONS
+  // Comment out this section to disable the conversion of the allocation selection dropdown to buttons
   $("#allocation-select").parent().append("<div class='allocation-btns'></div>")
 
   $("#allocation-select option").each(function() {
-    if ($(this).val() != "UNALLOCATED") { // Don't show UNALLOCATED
       label = "<strong>"+$(this).text().replace(/\: /g, "</strong><br>")
       var btn = $('<div class="btn btn-easi-toggle" data-value="'+$(this).val()+'"><span>'+label+'</span></div>');
       $('.allocation-btns').append(btn);
-    }
   });
+  
   $($('.allocation-btns .btn')[0]).addClass('on');
   $("#allocation-select").hide()
 
@@ -17,8 +17,10 @@
       $(this).addClass('on');
       $("#allocation-select").val($(this).data("value"));
   });
+  // END ALLOCATION BUTTONS
 
   // RESOURCE BUTTONS
+  // Comment out this section to disable the conversion of the resource selection dropdown to buttons
   $("#resources-select").parent().append("<div class='resources-btns'></div>")
 
   $("#resources-select option").each(function() {
@@ -37,7 +39,10 @@
       showInfo();
   });
 
+  // END RESOURCE BUTTONS
+
   // SPAWNER LOGOS
+  // Comment out this section to stop adding custom styling to spawner logos
   cols = $('.spawner-logos').closest('.col-md-11');
   cols.each(function(){
     $(this).removeClass('col-md-11');
@@ -51,10 +56,10 @@
   $('.spawner-logos .r-logo').append('<img src="/hub/static/extra-assets/images/r-logo.png">');
   $('.spawner-logos .python-logo').append('<img src="/hub/static/extra-assets/images/python-logo.png">');
   $('.spawner-logos .nvidia-logo').append('<img src="/hub/static/extra-assets/images/nvidia-logo.png">');
-
+  // END SPAWNER LOGOS
   
-
   // SPAWNER STYLING
+  // Coment out this section to stop adding custom styling to spawner groups
   $('#kubespawner-profiles-list').on('change', function() {
     $('#kubespawner-profiles-list input[type="radio"]').each(function(){
       if ($(this).is(':checked')) {
@@ -64,5 +69,6 @@
       }
     });
   })
+  // END SPAWNER STYLING
 
 })(jQuery);
